70940b31fe716c45335a8f23990a4f29eb487513
Julio1099 <juliio1520@gmail.com>
2025-06-02 13:17:04 -0300

    Adiciona Jogos ao Catalogo
    Co-authored-by: show-dawn <fernandogabrielcarriju@gmail.com>
    Co-authored-by: João Paulo Monteiro de Barros Ceva Rodrigues <jpceva@gmail.com>

--- docs/Códigos/Estrutural/CatalogoJogo.java
+++ docs/Códigos/Estrutural/CatalogoJogo.java
@@ -6,11 +6,17 @@ public class CatalogoJogo {
 
     public CatalogoJogo() {
         jogos.add(new Jogo("Minecraft", "aventura"));
-        jogos.add(new Jogo("Call of Duty", "acao"));
-        jogos.add(new Jogo("GTA V", "acao"));
+        jogos.add(new Jogo("Uncharted", "aventura"));
+        jogos.add(new Jogo("Mario", "plataforma"));
+        jogos.add(new Jogo("Sonic", "plataforma"));
+        jogos.add(new Jogo("Zelda", "aventura"));
+        jogos.add(new Jogo("Fallout", "aventura"));
+        jogos.add(new Jogo("GTA", "acao"));
+        jogos.add(new Jogo("Halo", "acao"));
         jogos.add(new Jogo("Outlast", "terror"));
         jogos.add(new Jogo("Resident Evil", "terror"));
-        jogos.add(new Jogo("Stardew Valley", "simulacao"));
+        jogos.add(new Jogo("Roblox", "simulacao"));
+        jogos.add(new Jogo("Sims", "simulacao"));
     }
 
     public List<Jogo> buscarPorTag(String tag) {
